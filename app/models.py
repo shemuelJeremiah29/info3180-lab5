3b4e6a6b85d9bd21b2231392dbe55932a6e3d29e
Andrew Morris <andrew.morris03@mymona.uwi.edu>
2018-03-07 15:34:45 +0000

    Add Password Field Completed

--- app/models.py
+++ app/models.py
@@ -6,7 +6,9 @@ class UserProfile(db.Model):
     first_name = db.Column(db.String(80))
     last_name = db.Column(db.String(80))
     username = db.Column(db.String(80), unique=True)
-    password = db.Column(db.String(8),unique=True) 
+    password = db.Column(db.String(255),unique=True) 
+    
+    
     def is_authenticated(self):
         return True
 
